e87a55aa9ef93fcb8b6fc35a17edaa062d09f905
scaasic <cameron@sidespark.com>
2017-07-25 17:00:22 -0500

    Holy shit it renders

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  NavLink
 } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Nav />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Nav () {
+  return(
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/discover">Discover</NavLink>
+      <NavLink to="/login">Sign In</NavLink>
+    </nav>
+  )
+}
+
+export default Nav;
