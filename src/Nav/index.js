1cd5fa8d091126d7477b9521967420db587b41bc
scaasic <cameron@sidespark.com>
2017-08-03 12:15:25 -0500

    Remove duplicate create link

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -6,7 +6,6 @@ function Nav ({ auth }) {
     <nav className="navbar">
       <div className="navbar-section">
         <Link to="/discover" className="btn btn-link">Discover</Link>
-        <Link to="/create" className="btn btn-link">Create</Link>
       </div>
       <div className="navbar-center">
         <Link to="/" className="navbar-brand mr-10">Sidespark</Link>
