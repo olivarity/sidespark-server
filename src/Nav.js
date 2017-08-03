37ddb742d1ad5a37dcdfba8fe7d51c90db8369fa
scaasic <cameron@sidespark.com>
2017-08-03 11:34:24 -0500

    Better choice of HTML tags

--- src/Nav.js
+++ src/Nav.js
@@ -4,18 +4,18 @@ import './spectre.min.css'
 
 function Nav () {
   return (
-    <header className="navbar">
-      <section className="navbar-section">
+    <nav className="navbar">
+      <div className="navbar-section">
         <Link to="/discover" className="btn btn-link">Discover</Link>
         <Link to="/create" className="btn btn-link">Create</Link>
-      </section>
-      <section className="navbar-center">
+      </div>
+      <div className="navbar-center">
         <Link to="/" className="navbar-brand mr-10">Sidespark</Link>
-      </section>
-      <section className="navbar-section">
+      </div>
+      <div className="navbar-section">
         <Link to="/login" className="btn btn-sm btn-primary">Sign In</Link>
-      </section>
-    </header>
+      </div>
+    </nav>
   )
 }
 
