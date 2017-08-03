5375e3a99172eeabaf73398e6746cddec34a771f
scaasic <cameron@sidespark.com>
2017-08-02 23:10:52 -0500

    Center home link

--- src/Nav.js
+++ src/Nav.js
@@ -6,9 +6,11 @@ function Nav () {
   return (
     <header className="navbar">
       <section className="navbar-section">
-        <Link to="/" className="navbar-brand mr-10">Sidespark</Link>
         <Link to="/discover" className="btn btn-link">Discover</Link>
         <Link to="/create" className="btn btn-link">Create</Link>
+      </section>
+      <section className="navbar-center">
+        <Link to="/" className="navbar-brand mr-10">Sidespark</Link>
       </section>
       <section className="navbar-section">
         <Link to="/login" className="btn btn-sm btn-primary">Sign In</Link>
