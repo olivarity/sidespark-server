c13bd87f31b96b62d58a0c586dff0f0e60abd31d
scaasic <cameron@sidespark.com>
2017-08-11 09:44:02 -0500

    oh wow react is still here?

--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -11,7 +11,7 @@ function Nav () {
         <Link to="/" className="navbar-brand mr-10">Sidespark</Link>
       </div>
       <div className="navbar-section">
-        <a href="https://sidespark.slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.avatar&client_id=***REMOVED***"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
+        <a href="/connect/slack"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
       </div>
     </nav>
   )
