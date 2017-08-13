a575c397851b02dd262a3afcac0b7fda11f19b8b
scaasic <cameron@sidespark.com>
2017-08-13 15:49:36 -0500

    Move passport config to auth.js

---
+++ auth.js
@@ -0,0 +1,45 @@
+const config = require('./config.json');
+const SlackStrategy = require('passport-slack').Strategy;
+
+module.exports = function(passport, UserModel) {
+
+  passport.use(new SlackStrategy({
+      clientID: config.slack.key,
+      clientSecret: config.slack.secret,
+      scope: config.slack.scope,
+      callbackURL: config.server.protocol + config.server.host + config.slack.callback
+    }, (accessToken, refreshToken, profile, done) => {
+      const id = profile.id
+      const update = {
+        _id: id,
+        authToken: accessToken,
+        email: profile.user.email,
+        name: profile.user.name,
+        avatar: { // Has to be a better way to do this...
+          image_24: profile.user.image_24,
+          image_32: profile.user.image_32,
+          image_48: profile.user.image_48,
+          image_72: profile.user.image_72,
+          image_192: profile.user.image_192,
+          image_512: profile.user.image_512
+        }
+      }
+      UserModel.findByIdAndUpdate(id, update, {upsert: true}, function (err, doc) {
+        done(null, doc);
+      });
+    }
+  ));
+
+  passport.serializeUser(function(user, done) {
+    //Save user's id to session store
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    UserModel.findById(id, function (err, doc) {
+        done(null, doc);
+    })
+  });
+
+
+}
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const morgan = require('morgan');
 const path = require('path');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
-const SlackStrategy = require('passport-slack').Strategy;
 const passport = require('passport');
 
 //Configure middleware:
@@ -14,44 +13,6 @@ const config = require('./config.json');
 const UserModel = require('./models/user');
 const db = mongoose.connect(config.server.database, { useMongoClient: true });
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-passport.use(new SlackStrategy({
-    clientID: config.slack.key,
-    clientSecret: config.slack.secret,
-    scope: ['identity.basic', 'identity.email', 'identity.avatar'],
-    callbackURL: 'http://localhost:5000/connect/slack/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    const id = profile.id
-    const update = {
-      _id: id,
-      authToken: accessToken,
-      email: profile.user.email,
-      name: profile.user.name,
-      avatar: { // Has to be a better way to do this...
-        image_24: profile.user.image_24,
-        image_32: profile.user.image_32,
-        image_48: profile.user.image_48,
-        image_72: profile.user.image_72,
-        image_192: profile.user.image_192,
-        image_512: profile.user.image_512
-      }
-    }
-    UserModel.findByIdAndUpdate(id, update, {upsert: true}, function (err, doc) {
-      done(null, doc);
-    });
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  //Save user's id to session store
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  UserModel.findById(id, function (err, doc) {
-      done(null, doc);
-  })
-});
 
 //Add middleware to app:
 const app = express();
@@ -64,6 +25,8 @@ app.use(session({
   store: new MongoStore({ mongooseConnection: db })
 }));
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+require('./auth')(passport, UserModel);
 app.use(passport.initialize());
 app.use(passport.session());
 
