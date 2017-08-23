d3895ae4ddee49da540cb59ba0e2fc2ed1016c29
scaasic <cameron@sidespark.com>
2017-08-22 22:59:03 -0500

    Nav MVP

--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -5,15 +5,15 @@ function Nav ({ auth }) {
   return (
     <nav className="navbar">
       <div className="navbar-section">
-        <Link to="/discover" className="btn btn-link">Discover</Link>
-      </div>
-      <div className="navbar-center">
-        <Link to="/" className="navbar-brand mr-10">Sidespark</Link>
+        <Link to="/" className="navbar-brand">Sidespark</Link>
       </div>
       <div className="navbar-section">
         { auth
-          ? <a href="/connect/logout" className="btn btn-primary">Sign Out</a>
-          : <a href="/connect/slack"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
+          ? <div>
+              <Link to="/create" className="btn btn-link">Create New</Link>
+              <a href="/connect/logout" className="btn btn-link">Sign Out</a>
+            </div>
+          : <a href="/connect/slack" className="btn btn-primary btn-sm">Sign in with Slack</a>
         }
       </div>
     </nav>
