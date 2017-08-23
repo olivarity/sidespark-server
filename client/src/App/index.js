f0da45235e806d01da3829c45cb7a8b7f7cdad2f
scaasic <cameron@sidespark.com>
2017-08-22 23:06:24 -0500

    Reorganizing

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Redirect,
 } from 'react-router-dom';
 import '../spectre.min.css';
 
-import Nav from '../Nav';
+import Nav from '../components/Nav';
+
 
 class App extends Component {
   constructor(props) {
@@ -33,8 +35,11 @@ class App extends Component {
             ? <h1>You are signed in as {auth.name}</h1>
             : <h1>Welcome! Please sign in.</h1>
           } />
-          <Route path="/discover" render={() => <h1>Discover</h1>} />
-          <Route path="/create" render={() => <h1>Create</h1>} />
+          <Route path="/create" render={() =>
+            auth
+            ? <h1>Create Placeholder</h1>
+            : <Redirect to="/" />
+          } />
         </div>
       </Router>
     );
