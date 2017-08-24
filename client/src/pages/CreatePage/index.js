723d12afe39e7eec9b08a46477a57c0f72536665
scaasic <cameron@sidespark.com>
2017-08-24 15:42:09 -0500

    Minor text fixes

--- client/src/pages/CreatePage/index.js
+++ client/src/pages/CreatePage/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import TextInput from '../../components/TextInput';
 import SelectInput from '../../components/SelectInput';
